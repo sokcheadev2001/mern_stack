5f5f9e9063158454726258ed1853f86e21a3515e
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-03-02 20:43:06 +0700

    Project Structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 6000;
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/routes"));
 app.listen(port, () => console.log(`Server started at port ${port}`));
