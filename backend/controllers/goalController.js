6171ed9a8715f5272237edf02f9ce8804fbe5c8c
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-03-08 19:10:22 +0700

    authorize user and goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const Goal = require("../models/goalModel");
 const { default: mongoose } = require("mongoose");
-
+const User = require("../models/userModel");
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -20,6 +20,7 @@ const setGoal = asyncHandler(async (req, res) => {
   }
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
   res.status(200).json(goal);
 });
@@ -28,30 +29,58 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route   PUT /api/goals/id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  const goal = Goal.findById(req.params.id);
+  const goal = await Goal.findById(req.params.id);
+
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
   }
-  const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+
+  // Check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
-  res.status(200).json(updateGoal);
+
+  res.status(200).json(updatedGoal);
 });
 
 // @desc    Get goals
 // @route   DELETE /api/goals/id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  const goal = Goal.findById(req.params.id);
+  const goal = await Goal.findById(req.params.id);
 
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
   }
 
-  const deletedGoal = await Goal.findByIdAndRemove(req.params.id);
-  res.status(200).json({ message: `Goal ${req.params.id} Deleted` });
+  // Check for user
+  if (!req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  await Goal.findByIdAndRemove(req.params.id);
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,30 +5,87 @@ const User = require("../models/userModel"); //  user model
 // @desc    Register user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please add all field");
   }
+
+  // check if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
   res.json({ message: "Register User" });
-};
+});
 
 // @desc    Authentication user
 // @route   POST /api/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
   res.json({ message: "Login User" });
-};
+});
 
 // @desc    Get user data
 // @route   POST /api/user/me
 // @access  Private
-const getUser = (req, res) => {
-  res.json({ message: "User data display" });
-};
+const getUser = asyncHandler(async (req, res) => {
+  const { _id, name, email } = await User.findById(req.user.id);
 
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
+});
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 module.exports = {
   registerUser,
   loginUser,
